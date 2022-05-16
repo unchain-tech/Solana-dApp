ec7ac43d51d91c1bd6be22d4f7f0949ad8ed3dfe
yuki4san <yuki.shimura@webempath.com>
2022-05-16 18:02:09 +0900

    starter kit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Create your dApp.
+
+![](public/ETH-banner-blur.png)
+
+このコースを始めるには、下記のステップを実行してください。
+
+1. あなたのGithubアカウントにこのレポジトリをフォークしましょう。
+
+2. クローンしたレポジトリをあなたのローカル環境にダウンロードしましょう。
+
+3. ターミナルを開き、ディレクトリのルートで `npm install` を実行します。
+
+4. `npm run start` を実行してプロジェクトを開始します。
+
+5. コーディングを始めましょう。
+
+----
+Banner logo is created by modifying "ETH diamond" © [Ethereum Foundation](https://ethereum.org/en/assets/) (Licensed under CC BY 4.0).
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// 定数を宣言します。
+const TWITTER_HANDLE = 'あなたのTwitterハンドル';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">
+            View your GIF collection ✨
+          </p>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
