b80699dcf133cd77a76b67e6e06ca92e8baf8fdb
github honganji <yujicavs@icloud.com>
2023-04-28 19:23:19 +0900

    fix test

--- packages/contract/tests/myepicproject.js
+++ packages/contract/tests/myepicproject.js
@@ -1,49 +1,59 @@
-const assert = require('assert');
 const anchor = require('@project-serum/anchor');
+const { assert } = require('chai');
 const { SystemProgram } = anchor.web3;
 
-describe('Solana-dApp-test', async () => {
-  console.log('🚀 Starting test...');
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+describe('myepicproject', () => {
+  it('Is initialized!', async () => {
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.Myepicproject;
-
-  const baseAccount = anchor.web3.Keypair.generate();
-  const tx = await program.rpc.startStuffOff({
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId,
-    },
-    signers: [baseAccount],
-  });
-  console.log('📝 Your transaction signature', tx);
-
-  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString());
-  assert.ok(typeof account.totalGifs === 'string');
-
-  const _baseAccount = baseAccount;
-  const _initialGifs = account.totalGifs;
-
-  it("It adds a GIF to the account's list", async () => {
-    const baseAccount = _baseAccount;
-    const initialGifs = _initialGifs;
-
-    // GIFリンクと送信ユーザーのアドレスを渡します。
-    await program.rpc.addGif('insert_a_gif_link_here', {
+    const program = anchor.workspace.Myepicproject;
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.startStuffOff({
       accounts: {
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
       },
+      signers: [baseAccount],
     });
+
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey,
+    );
+    // console.log('👀 GIF Count', account.totalGifs.toString());
+    assert.equal(
+      account.totalGifs.toString(),
+      '0',
+      'number of gifs should be 0',
+    );
+
+    // GIFリンクと送信ユーザーのアドレスを渡します。
+    await program.rpc.addGif(
+      'https://media.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy-downsized-large.gif',
+      {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      },
+    );
 
     // アカウントを呼び出します。
     account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    assert.ok(account.totalGifs === initialGifs + 1);
+    // console.log('👀 GIF Count', account.totalGifs.toString());
+    assert.equal(
+      account.totalGifs.toString(),
+      '1',
+      'number of gifs should be 1',
+    );
 
-    // アカウントでgif_listにアクセスします。
-    console.log('👀 GIF List', account.gifList);
+    // console.log('👀 GIF List', account.gifList);
+    assert.equal(
+      account.gifList[0].gifLink,
+      'https://media.giphy.com/media/R6gvnAxj2ISzJdbA63/giphy-downsized-large.gif',
+      'wrong gif information is added',
+    );
   });
 });
