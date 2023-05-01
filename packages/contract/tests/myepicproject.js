026c96b5d32b0198b2d54d035b151b1c9b1cdbbe
github honganji <yujicavs@icloud.com>
2023-05-02 08:47:55 +0900

    Revert "fix wallet path and content of test"
    This reverts commit ad71f732440ef39bc11fb1ffafab3b6946850939.

--- packages/contract/tests/myepicproject.js
+++ packages/contract/tests/myepicproject.js
@@ -12,6 +12,15 @@ describe('Solana-dApp-test', async () => {
 
     const baseAccount = anchor.web3.Keypair.generate();
 
+    console.log('📝 Your transaction signature', tx);
+
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey,
+    );
+    console.log('👀 GIF Count', account.totalGifs.toString());
+    assert.ok(typeof account.totalGifs === 'string');
+    const _initialGifs = account.totalGifs;
+
     const tx = await program.rpc.startStuffOff({
       accounts: {
         baseAccount: baseAccount.publicKey,
@@ -20,16 +29,7 @@ describe('Solana-dApp-test', async () => {
       },
       signers: [baseAccount],
     });
-
-    console.log('📝 Your transaction signature', tx);
-
-    let account = await program.account.baseAccount.fetch(
-      baseAccount.publicKey,
-    );
-    const _initialGifs = account.totalGifs;
-    const initialGifs = parseInt(_initialGifs);
-    console.log('👀 GIF Count', initialGifs.toString());
-    assert.ok(typeof initialGifs === 'number');
+    const initialGifs = _initialGifs;
 
     // GIFリンクと送信ユーザーのアドレスを渡します。
     await program.rpc.addGif('insert_a_gif_link_here', {
@@ -41,7 +41,7 @@ describe('Solana-dApp-test', async () => {
 
     // アカウントを呼び出します。
     account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    assert.ok(parseInt(account.totalGifs) === initialGifs + 1);
+    assert.ok(account.totalGifs === initialGifs + 1);
 
     // アカウントでgif_listにアクセスします。
     console.log('👀 GIF List', account.gifList);
