dc3383723449a85e8d09d915fc3b3ecb2a0aea52
github honganji <yujicavs@icloud.com>
2023-05-29 11:00:16 +0900

    add contract

--- packages/contract/tests/myepicproject.js
+++ packages/contract/tests/myepicproject.js
@@ -1,12 +1,12 @@
 const anchor = require("@project-serum/anchor");
 
-describe("solana-d-app", () => {
+describe("myepicproject", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const program = anchor.workspace.SolanaDApp;
+    const program = anchor.workspace.Myepicproject;
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
